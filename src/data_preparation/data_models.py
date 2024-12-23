eca3109dd57ae4f43e5b2e741b0956924c305551
PH <jnes40722@gmail.com>
2024-12-23 16:55:11 +0900

    Fix data model

--- src/data_preparation/data_models.py
+++ src/data_preparation/data_models.py
@@ -1,13 +1,8 @@
 import uuid
 
-from sqlalchemy import (
-    Column,
-    ForeignKey,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import Mapped, declarative_base, mapped_column, declared_attr, relationship
 
 Base = declarative_base()
 
@@ -15,7 +10,7 @@ Base = declarative_base()
 class KoreanVerbTable(Base):
     """Major tables to store the korean verbs."""
 
-    __tablename__ = "korean_verb"
+    __tablename__ = "korean_verbs"
 
     id = Column(
         Integer,
@@ -44,34 +39,45 @@ class KoreanVerbVarianceBaseTable(Base):
 
     __abstract__ = True
 
-    id = Column(
-        Integer,
-        primary_key=True,
-        autoincrement=True,
-        comment="Unique ID",
-    )
+    @declared_attr
+    def id(self) -> Mapped[int]:
+        """Unique ID."""
+        return mapped_column(
+            Integer,
+            primary_key=True,
+            autoincrement=True,
+            comment="Unique ID",
+        )
 
-    korean_verb_uuid = Column(
-        UUID(as_uuid=True),
-        ForeignKey("korean_verbs.korean_verb_uuid"),
-        nullable=False,
-        comment="Foreign key referencing korean_verb_uuid in KoreanVerbTable",
-    )
+    @declared_attr
+    def korean_verb_uuid(self) -> Mapped[UUID]:
+        """Foreign key referencing korean_verb_uuid in KoreanVerbTable."""
+        return mapped_column(
+            UUID(as_uuid=True),
+            ForeignKey("korean_verbs.korean_verb_uuid"),
+            nullable=False,
+            comment="Foreign key referencing korean_verb_uuid in KoreanVerbTable",
+        )
 
-    audio = Column(
-        String,
-        nullable=True,
-        comment="Path to the Korean verb variance audio.",
-    )
+    @declared_attr
+    def audio(self) -> Mapped[String]:
+        """Path to the Korean verb variance audio."""
+        return mapped_column(
+            String,
+            nullable=True,
+            comment="Path to the Korean verb variance audio.",
+        )
 
-    # Relationship to link back to the KoreanVerbTable
-    verb = relationship("KoreanVerbTable", back_populates="variances")
+    @declared_attr
+    def verb(self) -> Mapped["KoreanVerbTable"]:
+        """Relationship to link back to the KoreanVerbTable."""
+        return relationship("KoreanVerbTable", back_populates="variances")
 
 
 class KoreanVerbVarianceNegativeTable(KoreanVerbVarianceBaseTable):
     """A table to store korean verb variance (negative)."""
 
-    __tablename__ = "korean_verb_variances_negative"
+    __tablename__ = "korean_verbs_variances_negative"
 
     korean_verb_variance_negative = Column(
         String,
