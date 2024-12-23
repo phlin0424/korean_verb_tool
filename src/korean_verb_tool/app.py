b80388b25aee34f3fa8c10baef89c8b07f4d01cd
PH <jnes40722@gmail.com>
2024-12-23 14:26:07 +0900

    API modules

--- src/korean_verb_tool/app.py
+++ src/korean_verb_tool/app.py
@@ -1,40 +1,12 @@
-import json
-from pathlib import Path
+from fastapi import FastAPI
 
-import streamlit as st
+from korean_verb_tool.utils import setup_logging
+from korean_verb_tool.routers import routers
 
-# Open and read the JSON file
-# TODO: need to be replaced into a artifact data storage
-DIR_PATH = Path(__file__).resolve().parent.parent
-mp3_path = DIR_PATH / "data"
+setup_logging()
+app = FastAPI()
+app.include_router(routers)
 
-with (mp3_path / "test.json").open(encoding="utf-8") as json_file:
-    data = json.load(json_file)
-
-
-def mp3_player(text: str, mp3_filename: Path) -> None:
-    """Display the mp3 player widget.
-
-    Args:
-        text (str): _description_
-        mp3_filename (Path): _description_
-    """
-    # MP3 file to play
-    audio_file_path = mp3_path / mp3_filename
-
-    # Load and play the audio file
-    with audio_file_path.open("rb") as audio_file:
-        audio_bytes = audio_file.read()
-
-    st.text(text)
-    st.audio(audio_bytes, format="audio/mp3")
-
-
-# Set the title
-st.title("Simple MP3 Player")
-
-# Streamlit's audio player
-for item in data["data"]:
-    word = item["word"]
-    mp3_filename = item["mp3"]
-    mp3_player(word, mp3_filename)
+# If deploying to Lambda
+# from mangum import Mangum
+# handler = Mangum(app)
