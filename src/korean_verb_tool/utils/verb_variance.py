c99b675d4016fea819a29c7c6691c1b34e5b7f14
PH <jnes40722@gmail.com>
2025-01-01 09:50:58 +0900

    Function to generate negative variance form

--- src/korean_verb_tool/utils/verb_variance.py
+++ src/korean_verb_tool/utils/verb_variance.py
@@ -1,13 +1,22 @@
 from pydantic_ai import Agent
+from pydantic_ai.models.gemini import GeminiModel
+
 from korean_verb_tool.config import settings
 from korean_verb_tool.utils.prompt_manager import PromptManager
-from pydantic_ai.models.gemini import GeminiModel
 
 # TODO: Enable other types of models also: https://ai.pydantic.dev/models/
 model = GeminiModel("gemini-1.5-flash", api_key=settings.gemini_api_key)
 
 
 async def generate_negative_variance(verb: str) -> str:
+    """Function to generate negative form of the input korean verb.
+
+    Args:
+        verb (str): Korean verb.
+
+    Returns:
+        str: _description_
+    """
     # Prompt for generating negative form of the specific verb
     prompt = PromptManager().negative_verb_prompt
 
@@ -17,4 +26,5 @@ async def generate_negative_variance(verb: str) -> str:
     # Make a request
     result = await agent.run(verb)
 
-    return result.data
+    # Remove \n symbol if exists
+    return result.data.split("\n")[0]
