ba487372ab13adfaa7c8d878aa9f5a4e24962674
PH <jnes40722@gmail.com>
2025-01-07 19:59:49 +0900

    Move to cruds

--- src/korean_verb_tool/db/cruds.py
+++ src/korean_verb_tool/db/cruds.py
@@ -1,4 +1,5 @@
 import uuid
+from abc import ABC, abstractmethod
 
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
@@ -8,71 +9,7 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 from korean_verb_tool.config import settings
 from korean_verb_tool.db.base import KoreanVerbTable, KoreanVerbVarianceBaseTable
 from korean_verb_tool.utils.audio import AudioCreator
-
-
-async def create_korean_verb(
-    db: AsyncSession,
-    korean_verb: str,
-) -> KoreanVerbTable:
-    """Inserting rows to KoreanVerbTable.
-
-    Args:
-        db (AsyncSession): _description_
-        korean_verb (str): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    new_verb = KoreanVerbTable(
-        korean_verb=korean_verb,
-        korean_verb_uuid=uuid.uuid5(
-            settings.namespace_uuid,
-            korean_verb,
-        ),
-    )
-    db.add(new_verb)
-    await db.commit()
-    await db.refresh(new_verb)
-    return new_verb
-
-
-async def create_korean_variance(
-    db: AsyncSession,
-    table_model: type[DeclarativeMeta],
-    korean_variance: str,
-    relationship_table: KoreanVerbTable,
-) -> KoreanVerbTable:
-    """Generic function to insert a row into a korean variance table.
-
-    Args:
-        db (AsyncSession): _description_
-        table_model (type[DeclarativeMeta]): _description_
-        korean_variance (str): _description_
-        relationship_table (KoreanVerbTable): _description_
-
-    Returns:
-        KoreanVerbTable | KoreanVerbVarianceBaseTable: _description_
-    """
-    try:
-        # Create the audio
-        audio_creator = AudioCreator()
-        mp3filename = audio_creator.create_audio(korean_variance)
-
-        # Create a row using the predefined table model
-        new_row = table_model(
-            korean_verb_variance_negative=korean_variance,
-            audio=str(mp3filename.name),
-            verb=relationship_table,
-        )
-        db.add(new_row)
-        await db.commit()
-        await db.refresh(new_row)
-    except SQLAlchemyError:
-        await db.rollback()
-        raise
-
-    # Return the created table model, which is row has been inserted.
-    return new_row
+from korean_verb_tool.utils.verb_variance import generate_negative_variance
 
 
 async def get_row_by_verb(
@@ -105,44 +42,132 @@ async def get_row_by_verb(
     return result2.scalars().first()
 
 
-# async def update_row(
-#     db: AsyncSession,
-#     table_model: type[DeclarativeMeta],
-#     korean_verb: str,
-#     **kwargs,
-# ) -> type[DeclarativeMeta]:
-#     """Update a row in a table by korean verb.
+class BaseRepository(ABC):
+    """Base module for executing db CRUD. (async)."""
 
-#     Args:
-#         db (AsyncSession): The database session.
-#         table_model: The SQLAlchemy ORM table model class.
-#         row_id (int): The ID of the row to update.
-#         **kwargs: Column values to update (as key-value pairs).
+    def __init__(self, db: AsyncSession, main_table: type[DeclarativeMeta]) -> None:
+        self.db = db
+        self.main_table = main_table
 
-#     Returns:
-#         object: The updated row object, or None if the row does not exist.
-#     """
-#     try:
-#         # Fetch the row to be updated
-#         stmt = select(table_model).filter_by(korean_verb=korean_verb)
-#         result = await db.execute(stmt)
-#         row = result.scalars().first()
+    @abstractmethod
+    async def create() -> None:
+        pass
 
-#         if not row:
-#             return None  # Row does not exist
+    @abstractmethod
+    async def delete() -> None:
+        pass
 
-#         # Update the fields dynamically
-#         for key, value in kwargs.items():
-#             if hasattr(row, key):
-#                 setattr(row, key, value)
-#             else:
-#                 raise ValueError(f"Column '{key}' does not exist in {table_model.__name__}.")
+    # @abstractmethod
+    # async def update():
+    #     pass
 
-#         # Commit the changes
-#         await db.commit()
-#         await db.refresh(row)  # Refresh to get the updated values
-#         return row
+    # @abstractmethod
+    # async def get():
+    #     pass
 
-#     except SQLAlchemyError as e:
-#         await db.rollback()
-#         raise e
+    async def create_korean_verb(self, korean_verb: str) -> KoreanVerbTable:
+        """Inserting rows to KoreanVerbTable.
+
+        Args:
+            db (AsyncSession): _description_
+            korean_verb (str): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        new_verb = KoreanVerbTable(
+            korean_verb=korean_verb,
+            korean_verb_uuid=uuid.uuid5(
+                settings.namespace_uuid,
+                korean_verb,
+            ),
+        )
+        self.db.add(new_verb)
+        await self.db.commit()
+        await self.db.refresh(new_verb)
+        return new_verb
+
+    async def get_row_by_korean_verb(self, korean_verb: str) -> KoreanVerbTable:
+        """Get the row by specifying korean verb.
+
+        Args:
+            korean_verb (str): _description_
+
+        Returns:
+            KoreanVerbTable: _description_
+        """
+        # Create a select query
+        stmt = select(self.main_table).filter_by(korean_verb=korean_verb)
+        result = await self.db.execute(stmt)
+
+        return result.scalars().first()
+
+
+class NegativeVerbRepository(BaseRepository):
+    """Deal with negative form of korean crud."""
+
+    def __init__(
+        self,
+        db: AsyncSession,
+        main_table: type[DeclarativeMeta],
+        variance_table: type[DeclarativeMeta],
+    ) -> None:
+        """Initialize the class."""
+        super().__init__(db=db, main_table=main_table)
+        self.variance_table = variance_table
+
+    async def create_korean_variance(
+        self,
+        korean_variance: str,
+        relationship_table: KoreanVerbTable,
+    ) -> KoreanVerbTable:
+        """Generic function to insert a row into a korean variance table.
+
+        Args:
+            korean_variance (str): _description_
+            relationship_table (KoreanVerbTable): The main table row that related to the created variance row.
+
+        Returns:
+            KoreanVerbTable | KoreanVerbVarianceBaseTable: _description_
+        """
+        try:
+            # Create the audio
+            audio_creator = AudioCreator()
+            mp3filename = audio_creator.create_audio(korean_variance)
+
+            # Create a row using the predefined table model
+            new_row = self.variance_table(
+                korean_verb_variance_negative=korean_variance,
+                audio=str(mp3filename.name),
+                verb=relationship_table,
+            )
+            self.db.add(new_row)
+            await self.db.commit()
+            await self.db.refresh(new_row)
+        except SQLAlchemyError:
+            await self.db.rollback()
+            raise
+
+        # Return the created table model, which is row has been inserted.
+        return new_row
+
+    async def create(self, korean_verb: str) -> None:
+        """Insert a new korean verb into the main table and generates the corresponding variance and the audios.
+
+        Args:
+            korean_verb (str): string
+        """
+        # Generate verb variance using AI agent
+        korean_verb_negative = await generate_negative_variance(korean_verb)
+
+        # Insert into the main table
+        main_row = await self.create_korean_verb(korean_verb=korean_verb)
+
+        # Insert into the variance table
+        await self.create_korean_variance(
+            korean_variance=korean_verb_negative,
+            relationship_table=main_row,
+        )
+
+    async def delete(self, korean_verb: str) -> None:
+        pass
