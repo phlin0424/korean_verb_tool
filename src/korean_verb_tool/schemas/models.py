bf6170ce51c6e348f64e021da4395801f7ddd541
PH <jnes40722@gmail.com>
2025-01-26 14:20:37 +0900

    Add a query schema for posting to the DB

--- src/korean_verb_tool/schemas/models.py
+++ src/korean_verb_tool/schemas/models.py
@@ -18,3 +18,11 @@ class QueryResponse(BaseModel):
     origin: str = Field(default=None, description="Original form")
     audio: str = Field(default=None, description="The query result: Audio file name")
     variance: str = Field(default=None, description="The query result: the variance of the verb.")
+
+
+class PostQuery(BaseModel):
+    """A data model of a query to execute posting action to a variance table repository."""
+
+    origin: str = Field(default=None, description="Original form")
+    variance: str | None = Field(default=None, description="Negative form (or other variance) of the input verb")
+    audio: str | None = Field(default=None, description="Negative form (or other variance) of the input verb")
