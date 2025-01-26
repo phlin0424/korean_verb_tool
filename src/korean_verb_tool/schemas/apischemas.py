0b0860cbb4f2185213c19f3cbf730f9fab2d4f80
PH <jnes40722@gmail.com>
2025-01-26 14:18:58 +0900

    Add schemas for POST router

--- src/korean_verb_tool/schemas/apischemas.py
+++ src/korean_verb_tool/schemas/apischemas.py
@@ -2,10 +2,16 @@ from pydantic import BaseModel, Field
 
 
 class NegativeFormResponse(BaseModel):
-    origin: str = Field(default=None, description="韓国語の原型動詞（〜다）")
+    origin: str = Field(default=..., description="韓国語の原型動詞（〜다）")
     negative: str = Field(default=None, description="動詞の否定形")
     audio: str = Field(default=None, description="動詞の否定形の音声ファイル名")
 
 
 class VerbQuery(BaseModel):
-    origin: str = Field(default=None, description="韓国語の原型動詞（〜다）")
+    origin: str = Field(default=..., description="韓国語の原型動詞（〜다）")
+
+
+class VerbPost(BaseModel):
+    origin: str = Field(default=..., description="韓国語の原型動詞（〜다）")
+    negative: str | None = Field(default=None, description="動詞の否定形")
+    audio: str | None = Field(default=None, description="動詞の否定形の音声ファイル名")
